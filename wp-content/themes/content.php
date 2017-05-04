7adfaa8f10ad3c0a9f2b9a25fbfc8bb24f88b81c
husebyl <leigh.huseby@gmail.com>
2017-05-03 18:47:08 -0700

    first commit

---
+++ wp-content/themes/content.php
@@ -0,0 +1,16 @@
+<article id="post-<?php the_ID(); ?>" class="post-entry">
+	<div class="entry-wrap">
+		<header class="entry-header">
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?><?php the_date(); ?></a></h2>
+		</header>
+		<div class="entry-summary">
+			<?php the_content(); ?>
+		</div>
+		<footer class="entry-footer">
+			<div class="entry-meta">
+				<span class="entry-terms comments author"> Posted in <?php the_category(); ?>
+                                                             <?php echo get_comments_number(); ?> comments</span>
+			</div>
+		</footer>
+	</div>
+</article><?php the_title(); ?>
